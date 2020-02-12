139a439de6c57c6545f8f1562cb0a41772119119
elohhim <elohhim@gmail.com>
2020-02-12 23:28:16 +0100

    Updates

---
+++ precache-manifest.5b5423159a44b359b6432260b0174b49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c2bb950cb853fcc97fcb0b058ff7faf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27fe32eab67ded857820",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "27fe32eab67ded857820",
+    "url": "/static/js/2.0f7e7391.chunk.js"
+  },
+  {
+    "revision": "cc426e321724282b09241dfcfdad9a77",
+    "url": "/static/js/2.0f7e7391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d416c29ce0ce25e7e074",
+    "url": "/static/js/main.6c2332ac.chunk.js"
+  },
+  {
+    "revision": "4c7afb3a4cfe3cb8101e",
+    "url": "/static/js/runtime-main.5d2d1ad6.js"
+  }
+]);
