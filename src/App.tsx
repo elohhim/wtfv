8056af500de6d14114c9b12e41b6a3a9eed9d4a8
elohhim <elohhim@gmail.com>
2020-02-18 08:48:57 +0100

    Fixed few typos, added special handling for Thursday

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,27 @@ registerLocale('en-GB', enGB);
 
 const splitAt = (index: number, x: string): string[] => [x.slice(0, index), x.slice(index)];
 
+const getFixVersionAndPlus = (yearLittle: string, week: number, day: number): [string, string] => {
+  if (day === 4) {
+    return [
+        `${yearLittle}.${week + 1} or ${yearLittle}.${week + 2}`,
+        `1 or 2`
+    ];
+  } else {
+    const plus = day > 4 ? 2 : 1;
+    return [`${yearLittle}.${week + plus}`, `${plus}`];
+  }
+};
+
+const isoLink = <a href="https://en.wikipedia.org/wiki/ISO_8601">ISO 8601</a>;
+
 const App = () => {
   const [date, setDate] = useState(new Date());
   const {year, week, day} = weeknumber.weekNumberYear(date);
   const dayName = date.toLocaleString('en-US', {weekday: 'long'});
-  const plus = day > 4 ? 2 : 1;
   const [yearBig, yearLittle] = splitAt(2, year.toString());
-  const fixVersion = `${yearLittle}.${week + plus}`;
-
+  const [fixVersion, plus] = getFixVersionAndPlus(yearLittle, week, day);
+  const thursdayExplain = '(Thursdays are tricky, there is release to staging on Thursday, check if the release was done already)';
   return (
       <div className="App">
         <header>
@@ -27,11 +40,11 @@ const App = () => {
           <details>
             <summary>Explain...</summary>
             <p>
-              The year is {yearBig}
-              <mark>{yearLittle}</mark>
+              The year is {yearBig}<mark>{yearLittle}</mark>
               and it is <mark>{ordinal(week)} week</mark>
-              of the year (according to <a href="https://en.wikipedia.org/wiki/ISO_8601">ISO 8601</a>).
-              It is <mark>{dayName}</mark> so we need to add <mark>{plus}</mark> to the week number.
+              of the year (according to {isoLink}).
+              It is <mark>{dayName}</mark> {day === 4 ? thursdayExplain : ''} so we need to add <mark>{plus}</mark> to
+              the week number.
               Hence we have <mark>{fixVersion}</mark>.
             </p>
           </details>
@@ -53,8 +66,13 @@ const App = () => {
         </main>
         <footer>
           <ul>
-            <li>TBH I don't know if fix version uses ISO 8601 week numbering, it might be pure coincidence that it works for 2020w.</li>
-            <li>It might not work correctly around turn of the year.</li>
+            <li>TBH I don't know if fix version uses {isoLink} week numbering, it might be pure coincidence that it
+              works for 2020.
+            </li>
+            <li>
+              <del>It might not work correctly</del>
+              It definitely works incorrectly around turn of the year.
+            </li>
             <li>There is no reason for this to be React app... but it is.</li>
             <li>It uses <a href="https://kognise.github.io/water.css/">Water.css</a> (bundled cause official CDN was
               serving some old version).
