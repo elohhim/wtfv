c223c5d50f928b7214fa21639c12d5b38e36e09f
elohhim <elohhim@gmail.com>
2022-04-15 22:37:01 +0200

    update test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders What The Fix Version? header", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/What The Fix Version\?/i);
   expect(linkElement).toBeInTheDocument();
 });
